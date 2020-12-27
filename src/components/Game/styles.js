65d093bb3806185eb1a4fe9ae9cdcf71de6699e7
Reinaldo Ferreira <reinaldo.ferreira@meta.com.br>
2020-12-27 18:08:22 -0300

    criado componente game

---
+++ src/components/Game/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  ol,
+  ul {
+    padding-left: 30px;
+  }
+
+  /* .game {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .game-info {
+    margin-left: 20px;
+  } */
+`;
